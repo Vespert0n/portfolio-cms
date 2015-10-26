02e1cbe4f65459cf26e57de62b09973b2a398fcd
Drew Collins <drew@drewcollins.me>
2015-10-26 16:53:13 +1100

    added dependencies into example project

--- example/portfolio-cms/config.inc.php
+++ example/portfolio-cms/config.inc.php
@@ -2,7 +2,12 @@
 
 define('ADMIN_URL', BASE_URL.'admin/');
 define('CMS_URL', BASE_URL.'portfolio-cms/');
-define('JQUERY_URL', '../../dependencies/jquery-2.1.4.min.js');
-define('BOOTSTRAP_URL', '../../dependencies/bootstrap-3.3.5/');
+
+define('JS_URL', BASE_URL.'js/');
 define('CSS_URL', CMS_URL.'css/');
 define('IMAGE_URL', BASE_URL.'images/');
+
+define('JQUERY_URL', JS_URL.'jquery-2.1.4.min.js');
+define('BOOTSTRAP_URL', BASE_URL.'bootstrap-3.3.5/');
+define('CKEDITOR_URL', JS_URL.'ckeditor-4.5.3');
+define('KCFINDER_URL', JS_URL.'kcfinder-3.12');
