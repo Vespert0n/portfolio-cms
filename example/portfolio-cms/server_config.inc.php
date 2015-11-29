355801be727b882298bd88b6db63660646dff013
Drew Collins <drewcollinsme@users.noreply.github.com>
2015-11-30 09:51:31 +1100

    Delete server_config.inc.php

--- example/portfolio-cms/server_config.inc.php
+++
@@ -1,8 +0,0 @@
-<?
-define('BASE_URL', 'http://localhost/portfolio-cms/example/');
-define('BASE_DIR', 'C:/Development/portfolio-cms/example/');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'portfolio-cms');
